2427d128fc58401a43968826de661cb41b616fcf
Jaimedv17 <jaime.durand.vega@gmail.com>
2023-09-11 20:52:33 -0500

    ejercicio culminado

---
+++ add.js
@@ -0,0 +1,36 @@
+
+//........................1
+const person ={
+    nombre: "Jaime",
+    edad: "27",
+};
+console.log(person)
+//........................2.......
+
+console.log (person.nombre)
+
+//........................3.......
+
+console.log (person ) //se usa corchetes pero no pude colocarlo, mi teclado no me lo permite
+
+//........................4..
+
+person.apellido = "Durand"
+console.log (person.apellido)
+
+//........................5.....
+
+person.nombre = "Emanuel"
+console.log (person)
+
+
+//.......................6.........
+
+delete person.nombre;
+
+
+
+
+
+
+
